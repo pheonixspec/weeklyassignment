89fc070bd6eebc18ba92ecf68bc3b98fd13713fd
pheonixspec <kshitijattri@gmail.com>
2026-03-06 14:52:51 +0530

    marks

---
+++ q2.java
@@ -0,0 +1,12 @@
+public class q2 {
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+       
+        int totalMarks = maths + physics + chemistry;
+        double averageMark = totalMarks / 3.0;
+        
+        System.out.println("Sam’s average mark in PCM is " + averageMark);
+    }
+}
